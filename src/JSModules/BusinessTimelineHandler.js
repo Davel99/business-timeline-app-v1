1af1073bc2dbbd70c2b08c5df853a98323e704fe
davel99 <joelmp3@live.com>
2024-01-21 13:20:24 -0600

    Added main handler

---
+++ src/JSModules/BusinessTimelineHandler.js
@@ -0,0 +1,18 @@
+export default class BusinessTimelineHandler{
+    constructor(){
+
+    }
+
+    addUser(user){
+
+    }
+
+    addTimeline(timeline){
+
+    }
+
+    getTimelineAsColData(){
+        
+    }
+    
+}
