f9a83c5fc51738b9399e92ed69e15597b01a465e
davel99 <joelmp3@live.com>
2024-01-21 14:49:59 -0600

    Succesfully tested date creationin timeline

--- src/JSModules/utility/BusinessTimeline.js
+++ src/JSModules/utility/BusinessTimeline.js
@@ -3,9 +3,21 @@ export default class BusinessTimeline{
         this.timezone = timezone;
     }
 
-    calculateWorkTime(workTime){
-        let data = [];
-        this.calculatedWorkTime = data;
+    calculateWorkTime(workTimes){
+        let dates = [];
+        workTimes.forEach(workTime => {
+            let start = workTime.startTime;
+            let end = workTime.endTime;
+
+            let startDate = new Date();
+            let endDate = new Date();
+
+            startDate.setHours(start.hour, start.min, 0);
+            endDate.setHours(end.hour, end.min, 0);
+
+            dates.push([startDate, endDate]);
+        });
+        this.calculatedWorkTime = dates;
         return this.calculatedWorkTime;
     }
 }
